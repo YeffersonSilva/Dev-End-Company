1f69a79117f2031b3ebf7f824f5cc75c31e0d81b
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-05 22:59:29 -0300

    feat: Update food image and mock data for homepage components- Updated the food image in the homepage components to improve visual presentation.- Added new mock data to facilitate the testing and development of components.- Ensured proper integration between the new image and mock data, ensuring a consistent experience.

--- src/components/home/ProcessMap.jsx
+++ src/components/home/ProcessMap.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const ProcessMap = () => {
   return (
     <section className="max-w-[1200px] mx-auto py-[65px] text-white px-4">
-      <div className="grid lg:grid-cols-2 gap-6 items-center">
+      <div className="grid items-center gap-6 lg:grid-cols-2">
         <motion.div
           className="max-w-[584px] flex flex-col items-center lg:items-start text-center lg:text-start"
           initial={{
@@ -20,19 +20,25 @@ const ProcessMap = () => {
             ease: "easeInOut",
           }}
         >
-          <h3 className="lg:text-2xl md:text-xl text-lg font-semibold font-montserrat">
-            Try our FREE Process Map Generator
-          </h3>
-          <p className="mt-3 mb-10 font-source-sans lg:text-xl md:text-lg text-base">
-            Generate Process Flow maps and diagrams for your projects with
-            automated AI generation of User Stories.
-          </p>
-          <button
-            type="submit"
-            className="bg-risd-blue rounded-lg lg:min-h-[56px] min-h-[50px] text-nowrap md:text-base text-sm px-5 font-semibold hover:scale-105 transition-all duration-300 ease-in-out"
-          >
-            Try it Now For Free
-          </button>
+        <h3 className="text-lg font-semibold lg:text-2xl md:text-xl font-montserrat">
+  Soluciones Personalizadas para tu Negocio
+</h3>
+<p className="mt-3 mb-10 text-base font-source-sans lg:text-xl md:text-lg">
+  Desarrollamos sitios web y aplicaciones adaptadas a las necesidades específicas de tu empresa. ¡Habla con nosotros y encuentra la solución perfecta para tu negocio!
+</p>
+<a
+  href="https://wa.me/5555984595745"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <button
+    type="button"
+    className="bg-risd-blue rounded-lg lg:min-h-[56px] min-h-[50px] text-nowrap md:text-base text-sm px-5 font-semibold hover:scale-105 transition-all duration-300 ease-in-out"
+  >
+    Contáctanos por WhatsApp
+  </button>
+</a>
+
         </motion.div>
         <motion.div
           className="flex justify-center lg:justify-end"
